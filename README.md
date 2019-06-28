f616940db788a47dbf9efd2169d3693ce77c8464
Shadowsych <pravat.bhusal@gmail.com>
2019-06-27 21:34:19 -0600

    Create a README

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# FreeUT
-A React Native mobile app that notifies students of freebies and events nearby.
+# Frea
+An IOS and Android app that notifies people of freebies, events, parties, and many other things that are nearby their area.
+
+### Client-side
+Frea uses React Native for the client-side, specifically with [Expo](https://expo.io/).
+
+Install the client-side dependencies by running:
+```console
+npm install
+```
+
+To start the Expo client-side app, run:
+```console
+expo start
+```
+then follow the instructions on the console to load the app on your phone or emulator.
+
+### Server-side
+Frea uses [Express](https://expressjs.com/), [Socket.IO](https://socket.io/), and [MySQL](https://www.mysql.com/) for the server-side.
+- Express is used to run the web-server
+- Socket.IO is used to run the socket server, which wraps around the Express server
+- MySQL is the database management server
+
+Configure the server properties in the ```server.json``` file located in the root directory of the project.
+- ```serverDomain``` is the public domain of your web-server
+- ```serverPort``` is the port of your web-server
+- ```dbHost``` is the host of your MySQL database
+- ```dbUser``` is the user to access your MySQL database
+- ```dbPassword``` is the password of the user
+- ```dbName``` is the name of your MySQL database
+
+Then setup the database by importing the ```frea.sql``` file in MySQL. The file is located in the ```src/sql``` directory of the project.
+
+Once ```server.json``` is configured and the database is setup, install the server-side dependencies by running:
+```console
+cd src/server
+npm install
+```
+
+To start the server, run:
+```console
+cd src/server
+node Index.js
+```
+
+### Expo MapView
+Frea uses Expo's [MapView](https://docs.expo.io/versions/latest/sdk/map-view/), which is a Map component that uses Apple or Google Maps on IOS and Android phones.
+
+MapView will work when testing on the Expo client, but when publishing to the Apple or Google Play store the MapView will not render due to API key limitations and security purposes.
+
+In order for the map to render once publishing the app, follow the instructions from the Expo documentation [here](https://docs.expo.io/versions/latest/sdk/map-view/).
---
+++ src/sql/frea.sql
@@ -0,0 +1,92 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 27, 2019 at 09:29 PM
+-- Server version: 5.7.26-0ubuntu0.18.04.1
+-- PHP Version: 7.2.19-0ubuntu0.18.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `frea`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `AccountRecord`
+--
+
+CREATE TABLE `AccountRecord` (
+  `id` int(11) NOT NULL,
+  `token` varchar(256) DEFAULT NULL,
+  `name` varchar(256) DEFAULT NULL,
+  `email` varchar(256) DEFAULT NULL,
+  `password` varchar(256) DEFAULT NULL,
+  `profile_photo` varchar(256) DEFAULT NULL,
+  `points` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `MarkerRecord`
+--
+
+CREATE TABLE `MarkerRecord` (
+  `id` int(11) NOT NULL,
+  `author_id` int(11) DEFAULT '-1',
+  `author` varchar(64) DEFAULT NULL,
+  `title` varchar(24) DEFAULT NULL,
+  `description` varchar(128) DEFAULT NULL,
+  `longitude` double(10,6) NOT NULL DEFAULT '0.000000',
+  `latitude` double(10,6) NOT NULL DEFAULT '0.000000',
+  `picture` varchar(256) DEFAULT NULL,
+  `category` varchar(256) DEFAULT NULL,
+  `likes` json DEFAULT NULL,
+  `created_date` bigint(20) DEFAULT '0',
+  `expires` bigint(20) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `AccountRecord`
+--
+ALTER TABLE `AccountRecord`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `MarkerRecord`
+--
+ALTER TABLE `MarkerRecord`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `AccountRecord`
+--
+ALTER TABLE `AccountRecord`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `MarkerRecord`
+--
+ALTER TABLE `MarkerRecord`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
